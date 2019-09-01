ad55f73504dd437c2f0cda9b8bb69bbe985f4f60
Upadhyay <prayag.upd@gmail.com>
2019-08-31 23:11:37 -0700

    create random str

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 random string gen
 ------------------
 
+```bash
+lein repl
+
+random-str-gen.strgen=> (random-str-gen)
+"IIDFJJJ"
+
+random-str-gen.strgen=> (random-str-gen)
+"FKFELMOMNJRRUWVS"
+```
--- src/random_str_gen/strgen.clj
+++ src/random_str_gen/strgen.clj
@@ -1,9 +1,12 @@
 (ns random-str-gen.strgen)
 
 (defn random-str-gen []
-  (let [str-len (rand-int 10)]
-      (println "random")
-    )
-  )
+  (let [str-len (rand-int 20)]
+      (apply str (for [i (range 0 str-len)] (char (+ 65 (+ (rand-int 10) i)))))
+      ))
 
-(def start (random-str-gen))
+(def start 
+  (let [rs (random-str-gen)]
+    (println (apply str rs))
+  )
+)
