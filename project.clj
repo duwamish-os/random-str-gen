272d50e19c00456a4a643776d4adb2e9f00b2a0f
Upadhyay <prayag.upd@gmail.com>
2019-08-31 21:29:17 -0700

    add basic str-gen

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "random str gen"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "4.0.0"]
-                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]])
+                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
+  :main ^:skip-aot random-str-gen.strgen
+  )
---
+++ src/random_str_gen/strgen.clj
@@ -0,0 +1,9 @@
+(ns random-str-gen.strgen)
+
+(defn random-str-gen []
+  (let [str-len (rand-int 10)]
+      (println "random")
+    )
+  )
+
+(def start (random-str-gen))
