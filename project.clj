9e256634e7014b9de3f735288ce9be80f13e7dfc
Upadhyay <prayag.upd@gmail.com>
2019-08-31 20:56:13 -0700

    setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject random-str-gen "1.0.0"
+  :description "random str gen"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "4.0.0"]
+                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]])
